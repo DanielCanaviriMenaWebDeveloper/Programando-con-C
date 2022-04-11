c7c4040d562bac3140753cbfd2c7690f5e08ab7c
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2022-04-10 22:45:09 -0400

    La sentencia if

---
+++ 7. La sentencia if/laSentenciaIf.c
@@ -0,0 +1,26 @@
+/*
+	La sentencia if:
+	
+	if (Condicion)
+		Accion
+
+*/
+
+// Prueba de Divisibilidad
+
+#include<stdio.h>
+
+int main(){
+	
+	int n1, n2;
+	printf("Digite 2 numeros: ");
+	scanf("%i %i", &n1, &n2);
+	
+	if (n1 % n2 == 0){
+		printf("El numero %i es divisible entre %i", n1, n2);
+	}
+	
+	
+	
+	return 0;
+}
