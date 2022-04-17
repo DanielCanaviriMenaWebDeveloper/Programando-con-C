3cff8138ad576096b44a212a2ea8ee4019c71889
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2022-04-16 23:42:35 -0400

    La sentencia Switch en C

---
+++ 11. La sentencia switch/sentenciaSwitch.c
@@ -0,0 +1,46 @@
+/*
+	Sentencia switch()
+	
+	El selector solo puede ser un datos de tipo char o int
+	
+	switch (selector){
+		
+		case etiqueta1 : sentencias1; break;
+		case etiqueta2 : sentencias2; break;
+		case etiqueta3 : sentencias3; break;
+		default: sentencias;
+	}
+*/
+
+// Ejemplo del número
+
+#include<stdio.h>
+
+int main(){
+	
+	int numero;
+	
+	printf("Digite un numero entre (1 y 3): ");
+	scanf("%i", &numero);
+	
+	switch(numero){
+		
+		case 1: printf("Es el numero 1");
+		break;
+		
+		case 2: printf("Es el numero 2");
+		break;
+		
+		case 3: printf("Es el numero 3");
+		break;
+		
+		default: printf("No a digitado un numero correcto");
+		
+	}
+	
+	
+	
+	
+	return 0;
+}
+
