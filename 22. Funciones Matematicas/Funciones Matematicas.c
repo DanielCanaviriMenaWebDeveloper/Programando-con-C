7914e52980df11086ed998d022c7e33da6217363
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2022-05-10 23:36:45 -0400

    Funciones Matematicas

---
+++ 22. Funciones Matematicas/Funciones Matematicas.c
@@ -0,0 +1,32 @@
+/*
+	Funciones Matematicas: 
+	
+	ceil(x)    -> Redondea al proximo entero mas cercano.
+	fabs(x)    -> Devuelve el valor absoluto de x.
+	floor(x)   -> Redondea por defecto al entero mas proximo.
+	fmod(x,y)  -> Calcula el resto de la division de x/y.
+	pow(x,y)   -> Calcula x elevado a la potencia y.
+	sqrt(x)    -> Devuelve la raiz cuadrada de x.
+*/
+
+#include<stdio.h>
+void funcion_matematica();
+
+int main(){
+	
+	funcion_matematica();
+	
+	return 0;
+}
+
+void funcion_matematica(){
+	
+	float x, cambio=0;
+	
+	printf("Digite un numero: ");
+	scanf("%f", &x);
+	
+	cambio = ceil(x); // 5.7 -> 6 || 5.1 -> 6 || 5.9 - > 6 || Redondea al proximo entero mas cercano.
+	
+	printf("%.2f", cambio);
+}
