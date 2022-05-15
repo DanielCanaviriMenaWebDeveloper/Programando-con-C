711cc0d26c69875d1b2e353d6b940c0f90c80390
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2022-05-14 23:32:44 -0400

    Función Recursiva : Factorial de un número

---
+++ 25. Funcion Recursiva/Funcion Recursiva Factorial.c
@@ -0,0 +1,24 @@
+/*
+	Recursividad : 
+	
+	Una función recursiva es una función que se llama a si misma.
+	
+	1. Caso Base.
+	2. Caso Recursivo.
+	
+*/
+
+// Factorial
+
+#include<stdio.h>
+
+int main(){
+	
+	int numero;
+	
+	printf("Digite un numero: ");
+	scanf("%i", &numero);
+	
+	
+	return 0;
+}
