f5cec0f7d6ecf41869504277347d803637ea4934
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2022-05-24 23:53:58 -0400

    Arrays o Arreglos Unidimensionales: Arrays enteros

---
+++ 27. Arrays o Arreglos Unidimensionales/Arrays enteros.c
@@ -0,0 +1,22 @@
+/*
+	Arrays y Arreglos Unidimensionales
+	
+	- Arrays enteros              <-----
+	- Arrays flotantes
+	- Arrays de caracter
+	- Arrays de String o Palabras
+
+*/
+
+#include<stdio.h>
+#include<stdio.h>
+
+int main(){
+	
+	int a[5] = {1, 2, 3, 4, 5};
+	
+	printf("%i", a[3]);
+	
+	getch();	
+	return 0;
+}
