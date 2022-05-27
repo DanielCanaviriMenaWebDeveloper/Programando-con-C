48f0d0af1b9ecd025776feaa33d2bc5dbf54e32b
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2022-05-26 23:51:12 -0400

    Array de Caracteres

---
+++ 27. Arrays o Arreglos Unidimensionales/Arrays de caracteres.c
@@ -0,0 +1,24 @@
+/*
+	Arrays y Arreglos Unidimensionales
+	
+	- Arrays flotantes            
+	- Arrays de caracter          <=======
+	- Arrays de String o Palabras
+
+*/
+
+#include<stdio.h>
+#include<stdio.h>
+
+int main(){
+	
+	int i;
+	char a[5] = {'a', 'e', 'i', 'o', 'u'};
+	
+	for(i=0;i<5;i++){
+		printf("%c, ", a[i]);
+	}
+	
+	getch();	
+	return 0;
+}
