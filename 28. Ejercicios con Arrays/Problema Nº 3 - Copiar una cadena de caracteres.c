94960b48a61648f05066d4aa3a9b5cc23ee4eab4
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2022-05-28 23:47:39 -0400

    Problema Nº 3 - Copiando una cadena de caracteres

---
+++ 28. Ejercicios con Arrays/Problema Nº 3 - Copiar una cadena de caracteres.c
@@ -0,0 +1,27 @@
+/*
+	Problema Nº 3: 
+	
+	Hacer 2 arrays de cadena de caracteres, pedir el nombre al usuario 
+	guardarlo dentro de un array y luego, copiar el contenido de ese
+	array a otro.
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#define TAM 20
+
+int main(){
+	
+	char nombre1[TAM], nombre2[TAM];
+	
+	printf("Digite su nombre: ");
+	gets(nombre1);  // nombre1 = daniel
+	
+	// nombre2 = nombre1;
+	
+	strcpy(nombre2, nombre1);
+	
+	printf("Su nombre es: %s", nombre2);
+	
+	return 0;
+}
