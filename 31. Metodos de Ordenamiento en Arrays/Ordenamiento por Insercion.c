d0dfb721fe5afc815527aa88d8decda850d0648f
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2022-06-05 23:51:38 -0400

    Ordenamiento por Inserción

---
+++ 31. Metodos de Ordenamiento en Arrays/Ordenamiento por Insercion.c
@@ -0,0 +1,29 @@
+/*
+	Ordenamiento por Inserción
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	int a[5] = {3, 1, 4, 2, 5};
+	int i, pos, aux;
+	
+	for(i=0;i<5;i++){
+		pos = i;
+		aux = a[i];
+		while((pos>0)&&(aux<a[pos-1])){
+			a[pos] = a[pos-1];
+			pos--;
+		}
+		a[pos] = aux;
+	}
+	
+	printf("Ascendente\n");
+	for(i=0;i<5;i++){
+		printf("%i ", a[i]);
+	}
+	
+	getch();
+	return 0;
+}
